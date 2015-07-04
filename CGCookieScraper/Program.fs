13debe23a5b15cbee13a46af5786e137ebda07ac
David Neumann <davidjneumann@gmail.com>
2015-07-04 14:34:02 -0400

    Removed un-needed main.

--- CGCookieScraper/Program.fs
+++ CGCookieScraper/Program.fs
@@ -7,6 +7,10 @@ open OpenQA.Selenium.Support.UI
 let driver = new OpenQA.Selenium.Firefox.FirefoxDriver()
 
 let wait = new WebDriverWait(driver, System.TimeSpan.FromSeconds(10.))
+
+if (not (System.IO.File.Exists("credentials.txt"))) then
+    printfn "You need a credentials.txt with your username and password on the first 2 lines."
+    exit 1
 let username,password = 
     let fin = System.IO.File.ReadAllLines("credentials.txt")
     (fin.[0], fin.[1])
@@ -21,14 +25,8 @@ let setText (element:IWebElement) text = element.SendKeys(text)
 
 let findWID id = wait.Until(ExpectedConditions.ElementIsVisible(By.Id(id)))
 
-[<EntryPoint>]
-let main argv = 
-    if (not (System.IO.File.Exists("credentials.txt"))) then
-        printfn "You need a credentials.txt with your username and password on the first 2 lines."
-        exit 1
-    goto "https://cgcookiearchive.com/"
-    findWID "header-login-form-toggle" |> click
-    setText (findWID "user_login") username
-    setText (findWID "user_pass")  password
-    printfn "%A" argv
-    0 // return an integer exit code
+goto "https://cgcookiearchive.com/"
+findWID "header-login-form-toggle" |> click
+setText (findWID "user_login") username
+setText (findWID "user_pass")  password
+exit 0 // return an integer exit code
