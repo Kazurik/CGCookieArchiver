a56905fbc0bacfe493e6afcc5ea9fcc93a69eb42
David Neumann <davidjneumann@gmail.com>
2015-07-07 14:04:30 -0400

    Finished adding link saving and lesson discovery. PROGRAM COMPLETE

--- CGCookieScraper/Program.fs
+++ CGCookieScraper/Program.fs
@@ -13,7 +13,7 @@ type InputNodes =
 
 let driver = new OpenQA.Selenium.Firefox.FirefoxDriver()
 
-let wait = new WebDriverWait(driver, System.TimeSpan.FromSeconds(10.))
+let wait = new WebDriverWait(driver, System.TimeSpan.FromSeconds(4.5))
 
 if (not (System.IO.File.Exists("credentials.txt"))) then
     printfn "You need a credentials.txt with your username and password on the first 2 lines."
@@ -59,61 +59,103 @@ let rec parseContainer (elements:IWebElement seq) =
                   |> List.filter(fun c -> match c with | Container(name, nodes) -> Seq.length  nodes > 0 | _ -> false)
     List.append folders links
 
-let rec fout = System.IO.File.CreateText("out.txt")
-let rec printTree tree prefix =
-    match tree with
-    | Container(name, nodes) -> fprintf fout "%s-->%s\n" prefix name; nodes |> List.iter (fun n -> printTree n (prefix + "-->" + name))
-    | Link(url, name) -> fprintf fout "%s--:%s (%s)\n" prefix name url
 
-let clientDownload (url:string) extension=
-    let client = new WebClient()
-    let header = (driver.Manage().Cookies.AllCookies) |> Seq.fold (fun acum s -> acum + s.Name + "=" + s.Value + ";") ""
-    client.Headers.Add(HttpRequestHeader.Cookie, header)
-    let agent = string (driver.ExecuteScript("return navigator.userAgent"))
-    client.Headers.Add("user-agent", agent)
-    let stream = client.OpenRead(url)
-    let starti = 
-        if (url.Contains("/post_id")) then url.Substring(0, url.IndexOf("/?post_id")).LastIndexOf("/") + 1
-        else 0
-    let endi = url.IndexOf("/?post_id")
-    let len = endi - starti
-    let filename = 
-        if len > 0 then url.Substring(starti, len) + extension
-        else url.Substring(url.LastIndexOf("/") + 1)
-    use fout = System.IO.File.Create(System.IO.Path.Combine(System.Environment.CurrentDirectory, filename))
-    stream.CopyTo(fout)
+let printTree tree prefix =
+    let rec fout = System.IO.File.CreateText("out.txt")
+    let rec printTree tree prefix = 
+        match tree with
+        | Container(name, nodes) -> fprintf fout "%s-->%s\n" prefix name; nodes |> List.iter (fun n -> printTree n (prefix + "-->" + name))
+        | Link(url, name) -> fprintf fout "%s--:%s (%s)\n" prefix name url
+    printTree
 
-let downloadVideo () = 
-    try
-        //wait.Until(ExpectedConditions.ElementExists(By.ClassName("post-downloads-toggle"))).Click()
-        driver.FindElement(By.ClassName("post-downloads-toggle")).Click()
-        let videoLink = findWLX "//div[@class='post-downloads']/a" |> Seq.filter (fun e -> e.Text.Trim() = "HD Video")
-        if Seq.length videoLink > 0 then
-            videoLink |> Seq.iteri(fun i a -> clientDownload (a.GetAttribute("href")) ".zip")
-        else
-            failwith "Video not found"
-    with
-        _ -> try clientDownload ((findWT "source").GetAttribute("src")) ".mp4" with _ -> ()
+let makeNameSafe name = 
+    let invalids = Array.concat [Path.GetInvalidPathChars(); Path.GetInvalidFileNameChars()]
+    invalids |> Seq.fold (fun (str:string) c -> str.Replace(string c, ""))  name
 
-let downloadNoneVideoFiles () =
-    try
-        //wait.Until(ExpectedConditions.ElementExists(By.ClassName("post-downloads-toggle"))).Click()
-        driver.FindElement(By.ClassName("post-downloads-toggle")).Click()
-        let fileLink = findWLX "//div[@class='post-downloads']/a" |> Seq.filter (fun e -> e.Text.Trim() <> "HD Video")
-        fileLink |> Seq.iter (fun e -> printfn "%s" e.Text)
-        if Seq.length fileLink > 0 then
-            fileLink |> Seq.iter (fun a -> printfn "Saving"; clientDownload (a.GetAttribute("href")) "_files.zip"; printfn "Saved")
-    with _ -> ()
+let saveEntirePage url name path =
+    printfn "Saving page content for: %s" url  
+    let clientDownload path (url:string) extension=
+        let client = new WebClient()
+        let header = (driver.Manage().Cookies.AllCookies) |> Seq.fold (fun acum s -> acum + s.Name + "=" + s.Value + ";") ""
+        client.Headers.Add(HttpRequestHeader.Cookie, header)
+        let agent = string (driver.ExecuteScript("return navigator.userAgent"))
+        client.Headers.Add("user-agent", agent)
+        let stream = client.OpenRead(url)
+        let starti = 
+            if (url.Contains("/?post_id")) then url.Substring(0, url.IndexOf("/?post_id")-1).LastIndexOf("/") + 1
+            else 0
+        let endi = url.IndexOf("/?post_id")
+        let len = endi - starti
+        let filename = 
+            if len > 0 then url.Substring(starti, len) + extension
+            else url.Substring(url.LastIndexOf("/") + 1)
+        use fout = File.Create(Path.Combine(path, makeNameSafe filename))
+        stream.CopyTo(fout)
 
+    let downloadVideo path = 
+        printfn "Downloading video"
+        try
+            //wait.Until(ExpectedConditions.ElementExists(By.ClassName("post-downloads-toggle"))).Click()
+            driver.FindElement(By.ClassName("post-downloads-toggle")).Click()
+            let videoLink = findWLX "//div[@class='post-downloads']/a" |> Seq.filter (fun e -> e.Text.Trim() = "HD Video")
+            if Seq.length videoLink > 0 then
+                videoLink |> Seq.iteri(fun i a -> clientDownload path (a.GetAttribute("href")) ".zip")
+                driver.FindElement(By.ClassName("post-downloads-toggle")).Click()
+            else
+                failwith "Video not found"
+        with
+            _ -> try clientDownload path ((findWT "source").GetAttribute("src")) ".mp4" with _ -> ()
+
+    let downloadNoneVideoFiles path =
+        printfn "Downloading files"
+        try
+            //wait.Until(ExpectedConditions.ElementExists(By.ClassName("post-downloads-toggle"))).Click()
+            driver.FindElement(By.ClassName("post-downloads-toggle")).Click()
+            let fileLink = findWLX "//div[@class='post-downloads']/a" |> Seq.filter (fun e -> e.Text.Trim() <> "HD Video")
+            if Seq.length fileLink > 0 then
+                fileLink |> Seq.iter (fun a -> clientDownload path (a.GetAttribute("href")) "_files.zip")
+            driver.FindElement(By.ClassName("post-downloads-toggle")).Click()
+        with _ -> ()
+
+    let downloadPage path (name:string) =
+        printfn "Saving html"
+        let name = if name.EndsWith(".htm") then name else  name + ".htm"
+        let html = driver.FindElementByTagName("html").GetAttribute("innerHTML")
+        File.WriteAllText(Path.Combine(path, makeNameSafe name), html)
+
+    goto url
+    let path = Path.Combine(path, makeNameSafe name)
+    Directory.CreateDirectory(path) |> ignore
+    downloadVideo path
+    downloadNoneVideoFiles path
+    downloadPage path name
+    path
+                                
 let rec saveNodesToDisk path container =
     match container with 
     | Container(name, links) -> //Create direcotries and then save all the links data
-                                let newPath = System.IO.Path.Combine(path, name)
+                                printfn "saveNodesToDisk Handling container: %s" name
+                                let newPath = System.IO.Path.Combine(path, makeNameSafe name)
                                 System.IO.Directory.CreateDirectory(newPath) |> ignore
                                 links |> List.iter (fun l -> match l with | Link(_,_) -> saveNodesToDisk newPath l | _ -> ())
                                 links |> List.filter (fun l -> match l with | Container(_,_) -> true | _ -> false) |> List.iter (saveNodesToDisk newPath)
     | Link(url, name) -> //Save all the links data 
-                         File.CreateText(Path.Combine(path, name)) |> ignore
+                         printfn "saveNodesToDisk Handling link: %s" name
+                         goto url
+                         if url.ToLower().Contains("/lessons/") then //Handle all lesson pages at once
+                            let course = Seq.head (findWLX "//div[@class='additional-info']//a")
+                            saveNodesToDisk path (Link(course.GetAttribute("href"), course.Text))
+                         else                            
+                            printfn "saveNodesToDisk Parsing: %s" name
+                            //Check if we are on the TOC for a course otherwise save the one off page
+                            try
+                                if not ((Seq.head (findWLX "//div[@class='course-lessons']/h3") |> text).Contains("Lessons")) then failwith "UNEXPECTED PAGE!"
+                                let path = saveEntirePage url name path
+                                let lessonLinks = findWLX "//ol[@class='course-lessons-list']/li/a" |> Seq.map (fun e -> e.GetAttribute("href"), (e.Text.Trim())) |> Seq.toArray
+                                lessonLinks |> Seq.iter (fun (link,text) -> saveEntirePage link text path |> ignore)
+                            with | :? OpenQA.Selenium.WebDriverTimeoutException  ->
+                                //If lessons list can't be found assume we are on an one off page and save only this pages content to disk.
+                                saveEntirePage url name path |> ignore
 
 //Get all the links and file structure we will need for the export
 if (not (System.IO.File.Exists("bookmarks.html"))) then
@@ -121,9 +163,7 @@ if (not (System.IO.File.Exists("bookmarks.html"))) then
     exit 1
 goto ("file:///" + (System.IO.Path.Combine(System.Environment.CurrentDirectory, "bookmarks.html")))
 let eles = findsWX "/html/body/dl/dt"
-printfn "Count: %d" (Seq.length eles)
 let result = Container("Output", parseContainer eles)
-printTree result ""
 
 //Log in
 goto "https://cgcookiearchive.com/"
@@ -135,16 +175,7 @@ click (findWID "wp-submit")
 //Save all links with correct file structure to disc
 if (not (Directory.Exists(Path.Combine(Environment.CurrentDirectory, "Output")))) then
     Directory.CreateDirectory(Path.Combine(Environment.CurrentDirectory, "Output")) |> ignore
-
 saveNodesToDisk (Path.Combine(Environment.CurrentDirectory)) result
-
-//goto "https://cgcookiearchive.com/blender/lessons/4-4-4-exercise-throwing-ball/"
-//goto "https://cgcookiearchive.com/concept/lessons/1-software-tablets/"
-//goto "https://cgcookiearchive.com/unity/cgc-courses/crash-course-breakout-particles-mini-course/"
-//downloadVideo ()
-
-//Download all content related to our links
-//TODO: The actual downloading.
 
 System.Console.ReadLine() |> ignore
 driver.Close()
